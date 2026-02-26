4e384cd2943db9992915044c7a28d484c00b6ff8
idekferewf <skadeshopp@gmail.com>
2026-02-26 13:15:36 +0300

    добавлена обработка формы создания тега

--- src/components/tags/TagCreateModal.vue
+++ src/components/tags/TagCreateModal.vue
@@ -2,11 +2,13 @@
 import type { RadioGroupItem } from "@nuxt/ui"
 import { useRegle } from "@regle/core"
 import { minLength, required, withMessage } from "@regle/rules"
-import { computed, ref } from "vue"
+import { computed, nextTick, ref, useTemplateRef, watch } from "vue"
 
 defineProps<{
   description?: string
 }>()
+
+const open = defineModel<boolean>({ default: false })
 
 const { r$ } = useRegle(
   { label: "", color: "neutral" },
@@ -17,6 +19,8 @@ const { r$ } = useRegle(
     }
   }
 )
+
+const labelRef = useTemplateRef("label")
 
 const indicatorClasses = computed<string>(() => {
   if (r$.$value.color === "neutral") {
@@ -31,6 +35,13 @@ const radioItemsClasses = computed<string>(() => {
   }
   return `has-data-[state=checked]:border-${r$.$value.color}/50 has-data-[state=checked]:bg-${r$.$value.color}/10`
 })
+
+const onSubmit = async () => {
+  const { valid, data } = await r$.$validate()
+  if (!valid) return
+
+  console.log(data)
+}
 
 const radioItems = ref<RadioGroupItem[]>([
   {
@@ -54,31 +65,57 @@ const radioItems = ref<RadioGroupItem[]>([
     value: "neutral"
   }
 ])
+
+watch(open, () => {
+  if (!open.value) {
+    r$.$reset({ toInitialState: true })
+    return
+  }
+  nextTick(() => {
+    labelRef.value?.inputRef?.focus()
+  })
+})
 </script>
 
 <template>
   <UModal
+    v-model:open="open"
     title="Создание тега"
     :description="description ?? 'Заполните поля ниже, чтобы создай новый тег.'"
-    :ui="{ footer: 'justify-end', content: 'max-w-[400px]' }"
+    :ui="{ footer: 'justify-end', content: 'max-w-[400px]', body: '!pt-3.5 sm:!pt-[22px]' }"
   >
     <!-- Trigger -->
     <slot />
     <!-- /Trigger -->
 
     <template #body>
-      <UForm :schema="r$" :state="r$.$value" class="space-y-4">
+      <UForm :schema="r$" :state="r$.$value" @submit="onSubmit">
+        <!-- Label -->
         <UFormField
-          label="Название"
+          name="label"
           hint="*"
           help="Придумайте краткое и понятное название."
-          name="label"
           :ui="{ hint: 'text-error' }"
+          class="mb-4"
         >
-          <UInput v-model="r$.$value.label" placeholder="Введите название" class="w-full" />
+          <template #label>
+            <div class="inline-flex items-center gap-x-1.5">
+              <UIcon name="i-lucide-letter-text" class="size-4" />
+              <span>Название</span>
+            </div>
+          </template>
+          <UInput ref="label" v-model="r$.$value.label" placeholder="Введите название" class="w-full" />
         </UFormField>
+        <!-- /Label -->
 
-        <UFormField label="Цвет" name="color">
+        <!-- Color -->
+        <UFormField name="color" hint="*" help="Выберите подходящий цвет из списка.">
+          <template #label>
+            <div class="inline-flex items-center gap-x-1.5">
+              <UIcon name="i-lucide-paintbrush" class="size-4" />
+              <span>Цвет</span>
+            </div>
+          </template>
           <URadioGroup
             v-model="r$.$value.color"
             indicator="end"
@@ -87,15 +124,17 @@ const radioItems = ref<RadioGroupItem[]>([
             variant="card"
             :items="radioItems"
             :ui="{ fieldset: 'flex-wrap', indicator: indicatorClasses, item: radioItemsClasses }"
-          >
-          </URadioGroup>
+            class="mb-0"
+          />
         </UFormField>
+        <!-- /Color -->
+        <UButton type="submit" class="invisible hidden" />
       </UForm>
     </template>
 
     <template #footer>
-      <UButton label="Отмена" color="neutral" variant="outline" />
-      <UButton label="Сохранить" color="neutral" variant="solid" />
+      <UButton label="Отмена" color="neutral" variant="outline" @click="open = false" />
+      <UButton label="Сохранить" color="neutral" variant="solid" :disabled="!r$.$correct" @click="onSubmit" />
     </template>
   </UModal>
 </template>
